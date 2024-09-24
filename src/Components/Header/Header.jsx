e560d32d10524749dc3cec1aa036a5db24e606b0
Alexis Castillo <alexisct629@Alexiss-MacBook-Air.local>
2024-09-24 19:01:54 -0400

    weather card done but still not showing

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,35 @@
-function Header(){
-    return
-    <Header>HEADER</Header>;
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header({ handleAddClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="header logo" />
+      <p className="header__date_time">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add_clothes"
+      >
+        + Add Clothes
+      </button>
+      <div className="header__user_info">
+        <p className="header__username">Terrence Tegegne</p>{" "}
+        <img
+          src={avatar}
+          alt="Terrence Tegegne"
+          className="header__user_avatar"
+        />
+      </div>
+    </header>
+  );
 }
+
 export default Header;
