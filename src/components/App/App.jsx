bc69e413d3a918c8ceaa2b5c1251726305791a9a
Alexisct2212 <154010615+Alexisct2212@users.noreply.github.com>
2024-10-03 10:55:58 -0400

    just missing the error on console

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,65 +1,109 @@
-import {useEffect, useState} from "react"
-import './App.css';
-import Header from '../Header/Header';
-import { coordinates,APIKey } from "../../utils/Constants";
-import Main from '../Main/Main';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import { coordinates, APIKey } from "../../utils/Constants";
+import Main from "../Main/Main";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import Footer from "../Footer/Footer";
 import ItemModal from "../itemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/WeatherApi";
-function App(){
-    const [weatherData, setWeatherData] = useState({type:"", temp:{F:999, C:999}, city:""})
-    const [activeModal,setActiveModal] =useState("")
-    const [selectedCard, setSelectedCard] = useState("")
-    const handleAddClick =()=>{
-        setActiveModal("add-garment");
-    }
-    const handleCardClick = (card)=>{
-        setActiveModal("preview");
-        setSelectedCard(card)
-    }
-    const CloseActiveModal = ()=>{
-        setActiveModal("");
-    }
-    useEffect(()=>{
-        getWeather(coordinates, APIKey)
-        .then((data)=>{
-            const filteredData = filterWeatherData(data);
-            setWeatherData(filteredData)
-            
-        }).catch(console.error)
-    },[]);
+function App() {
+  const [weatherData, setWeatherData] = useState({
+    type: "",
+    temp: { F: 999, C: 999 },
+    city: "",
+  });
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+  const handleCardClick = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+  const CloseActiveModal = () => {
+    setActiveModal("");
+  };
+  useEffect(() => {
+    getWeather(coordinates, APIKey)
+      .then((data) => {
+        const filteredData = filterWeatherData(data);
+        setWeatherData(filteredData);
+      })
+      .catch(console.error);
+  }, []);
 
-    return (
-    <div className='page'>
-     <div className="page__content">
-        <Header handleAddClick={handleAddClick} weatherData={weatherData}/>
-        <Main weatherData={weatherData} handleCardClick={handleCardClick}/>
-     </div>
-     <ModalWithForm title="New garment" buttonText="Add garment" 
-     activeModal={activeModal} onClose={CloseActiveModal}>
-
-     <label htmlFor="name" className="modal__label">Name{""}</label>
-            <input type="text" className="modal__input" id="name" placeholder="Name"/>
-            <label htmlFor="imageURL" className="modal__label">Image{""}</label>
-            <input type="Url" className="modal__input" id="imageURL" placeholder="Image URL"/>
-            <fieldset className="modal__radio-buttons">
-                <legend className="modal__legend">Select the weather type:</legend>
-                <label htmlFor="hot" className=" modal__label_type_radio">
-                    <input type="radio" className="modal__radio-input" id="hot" name="weatherType"/>Hot
-                </label>
-                <label htmlFor="warm" className=" modal__label_type_radio">
-                    <input type="radio" className="modal__radio-input" id="warm" name="weatherType"/>Warm
-                </label>
-                <label htmlFor="cold" className="modal__label_type_radio">
-                    <input type="radio" className="modal__radio-input" id="cold" name="weatherType"/>Cold
-                </label>
-            </fieldset>
-     </ModalWithForm>
-     <ItemModal activeModal={activeModal} card={selectedCard} onClose={CloseActiveModal}/>
-     <Footer/>
-    </div>);
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header handleAddClick={handleAddClick} weatherData={weatherData} />
+        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+      </div>
+      <ModalWithForm
+        title="New garment"
+        buttonText="Add garment"
+        activeModal={activeModal}
+        onClose={CloseActiveModal}
+        isOpen={activeModal === "add-garment"}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name{""}
+        </label>
+        <input
+          type="text"
+          className="modal__input"
+          id="name"
+          placeholder="Name"
+        />
+        <label htmlFor="imageURL" className="modal__label">
+          Image{""}
+        </label>
+        <input
+          type="Url"
+          className="modal__input"
+          id="imageURL"
+          placeholder="Image URL"
+        />
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className=" modal__label_type_radio">
+            <input
+              type="radio"
+              className="modal__radio-input"
+              id="hot"
+              name="weatherType"
+            />
+            Hot
+          </label>
+          <label htmlFor="warm" className=" modal__label_type_radio">
+            <input
+              type="radio"
+              className="modal__radio-input"
+              id="warm"
+              name="weatherType"
+            />
+            Warm
+          </label>
+          <label htmlFor="cold" className="modal__label_type_radio">
+            <input
+              type="radio"
+              className="modal__radio-input"
+              id="cold"
+              name="weatherType"
+            />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
+      <ItemModal
+        activeModal={activeModal}
+        card={selectedCard}
+        onClose={CloseActiveModal}
+      />
+      <Footer />
+    </div>
+  );
 }
-
 
 export default App;
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,17 +1,19 @@
 import { Children } from "react";
-import "./ModalWithForm.css"
-function ModalWithForm({children, title, buttonText,activeModal,onClose}){
-    return(
-        <div className={`modal ${activeModal === "add-garment"&& "modal__opened"}`}>
-            <div className="modal__content">
-            <h2 className="modal__title">{title}</h2>
-            <button className="modal__close" onClick={onClose}></button>
+import "./ModalWithForm.css";
+function ModalWithForm({ children, title, buttonText, onClose, isOpen }) {
+  return (
+    <div className={`modal ${isOpen && "modal_opened"}`}>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button className="modal__close" onClick={onClose}></button>
         <form className="modal__form">
-            {children}
-            <button className="modal__submit" type="submit" disabled>{buttonText}</button>
+          {children}
+          <button className="modal__submit" type="submit" disabled>
+            {buttonText}
+          </button>
         </form>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
-export default ModalWithForm
+export default ModalWithForm;
