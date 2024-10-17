ba9587c28f9c1610ebe411f8622d74b777c2d2e4
Alexis Castillo <alexisct629@Alexiss-MacBook-Air.local>
2024-10-17 18:42:36 -0400

    create the adding the context for the temperature part 3/10

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,5 +1,7 @@
+import { useContext } from "react";
 import { weatherOptions, defaultWeatherOptions } from "../../utils/Constants";
 import "./WeatherCard.css";
+import { CurrentTemperatureUnitContext } from "../context/CurrentTemperatureUnitContext";
 function WeatherCard({ weatherData }) {
   const filteredOptions = weatherOptions.filter((option) => {
     return (
@@ -7,16 +9,19 @@ function WeatherCard({ weatherData }) {
       option.condition === weatherData.condition
     );
   });
-
+  
   let weatherOption;
   if (filteredOptions.length === 0) {
     weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
   } else {
     weatherOption = filteredOptions[0];
   }
+  const {currentTemperatureUnit}= useContext(CurrentTemperatureUnitContext);
+  const displayTemp =
+  currentTemperatureUnit === "F" ? weatherData.temp.F : weatherData.temp.C;
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <p className="weather-card__temp">{displayTemp} &deg; {currentTemperatureUnit}</p>
       <img
         src={weatherOption?.url}
         alt={`Card showing ${weatherOption?.day ? "day" : "night"} time ${
